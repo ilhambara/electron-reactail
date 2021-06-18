f90e46a1538472999213d36d1ad806ca64398ec3
ilhambara <g2a.ilhammubarok@gmail.com>
2021-06-18 07:29:54 +0700

    add: starter page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import logo from './logo.svg';
+import logoReact from './logo/react.svg';
+import logoElectron from './logo/electron.svg';
+import logoTailwind from './logo/tailwind.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="text-gray-50 text-center">
+      <div className="min-h-screen bg-gray-800 flex flex-col items-center justify-center">
+        <div className="flex items-end">
+          <img src={logoReact} className="" alt="logo" />
+          <img src={logoElectron} className="App-logo" alt="logo" />
+          <img src={logoTailwind} className="pl-2" alt="logo" />
+        </div>
+        <div className="mt-8 flex flex-col">
+          <p className="text-xl">Boilerplate for cross-platform apps using</p>
+          <code className="mt-4 rounded-md px-4 py-3 bg-gray-900 text-indigo-100 text-2xl">React + Electron + TailwindCSS</code>
+          <p className="mt-6 text-lg">
+            Coded by 👨‍💻{' '}
+            <a className="text-blue-300 font-bold underline" href="https://ilhambara.vercel.app" target="_blank" rel="noopener noreferrer">
+              Ilhambara
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
